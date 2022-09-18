956b0f1ed18d736352895d846a8acee9994a235d
Dorian Birch <206dorian@gmail.com>
2022-09-18 13:42:03 -0500

    got local storage to save!

--- Assets/script.js
+++ Assets/script.js
@@ -33,6 +33,22 @@ function updateBlox() {
 }
 updateBlox()
 
+$(".saveBtn").on("click", function(){
+var hour = $(this).siblings("textarea").attr("id")
 
+var text = $(this).siblings("textarea").val()
+console.log(text)
+console.log(hour)
 
+localStorage.setItem(hour, text)
+})
 
+//getting item from localstorage, persisting upon reload
+$("#9").val(localStorage.getItem("9"));
+$("#10").val(localStorage.getItem("10"));
+$("#11").val(localStorage.getItem("11"));
+$("#12").val(localStorage.getItem("12"));
+$("#13").val(localStorage.getItem("13"));
+$("#14").val(localStorage.getItem("14"));
+$("#15").val(localStorage.getItem("15"));
+$("#16").val(localStorage.getItem("16"));
